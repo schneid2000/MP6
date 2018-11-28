315c0621022863a78dcfcb7efca9bf58c91e9fd9
SethWyma <wyma2@illinois.edu>
2018-11-27 18:02:30 -0600

    first commit

---
+++ app/src/main/java/com/example/seth/cs196fa18/mp6/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.seth.cs196fa18.mp6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    // commit1
+}
